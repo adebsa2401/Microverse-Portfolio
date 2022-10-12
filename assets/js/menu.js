850ad33b20209b9f438d42e3c532bad61cc9ac16
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-10-12 15:44:08 +0100

    Add: highligh current menu section in nav

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -9,3 +9,20 @@ Array.from(document.getElementsByClassName('menu-item')).forEach((element) => {
     document.getElementById('mobile-menu').style.display = 'none';
   });
 });
+
+// highligh current menu section in nav (desktop)
+document.addEventListener('scroll', () => {
+  Array.from(document.getElementsByClassName('nav-section')).forEach((element) => {
+    const rect = element.getBoundingClientRect();
+
+    const isTopVisible = rect.top >= 0 && rect.top <= window.innerHeight;
+    const isSectionBodyVisible = rect.top <= 0 && rect.bottom >= window.innerHeight;
+
+    if (isTopVisible || isSectionBodyVisible) {
+      document.querySelector(`.nav-menu a[href='#${element.id}']`).style.textDecoration = 'underline';
+    } else {
+      document.querySelector(`.nav-menu a[href='#${element.id}']`).style.textDecoration = 'none';
+    }
+  });
+});
+
